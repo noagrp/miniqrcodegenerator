42ca79a5246850f57ce37ad27b9530331c706c25
noagrp <lihwlee11@gmail.com>
2026-09-14 11:43:37 +0800

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'miniqr-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './qrcode.min.js',
+  './manifest.webmanifest?v=1',
+  './favicon-32x32.png?v=1',
+  './icon-192.png?v=1',
+  './icon-512.png?v=1',
+  './apple-touch-icon.png?v=1'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
